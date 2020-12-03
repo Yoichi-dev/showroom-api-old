bbe45ce2d46e02ba16ffd77d63c5633f7516fb3e
Yoichi-dev <yoichi.developer@gmail.com>
2020-12-03 22:53:34 +0900

    アクセス分析追加・オリジン許可

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let helmet = require('helmet')
 
 let indexRouter = require('./routes/index');
 let apisRouter = require('./routes/api');
-// let analyzeRouter = require('./routes/analyze');
+let analyzeRouter = require('./routes/analyze');
 
 let app = express();
 
@@ -15,7 +15,7 @@ app.use(helmet())
 
 // CORSを許可する
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://niconico-showroom.yoichi.dev");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/apis', apisRouter);
-// app.use('/analyze', analyzeRouter);
+app.use('/analyze', analyzeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
