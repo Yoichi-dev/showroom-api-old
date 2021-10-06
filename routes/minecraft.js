6e4830e2f64146c841df7715a3a1064347c568f5
Yoichi-dev <yoichi.developer@gmail.com>
2021-10-07 00:40:40 +0900

    [Add] MinecraftユーザID取得追加

--- routes/minecraft.js
+++ routes/minecraft.js
@@ -3,11 +3,40 @@
 let express = require('express');
 let router = express.Router();
 let pinger = require('minecraft-pinger');
+let request = require('then-request');
+let common = require('../common');
 
 router.get('/', (req, res, next) => {
-  pinger.ping('minecraft.yoichi.dev', 4466, (error, result) => {
+  pinger.ping('minecraft.showroom-app.com', 4466, (error, result) => {
     error ? res.json({}) : res.json(result);
   })
 });
+
+// ユーザ情報取得
+router.get('/search/:user_id', common.asyncWrapper(async (req, res, next) => {
+
+  try {
+    let userData = await getApi(`https://api.mojang.com/users/profiles/minecraft/${req.params.user_id}`);
+    res.json(userData);
+  } catch {
+    res.json({
+      name: null,
+      id: null
+    });
+  }
+
+}));
+
+function getApi(url) {
+  return new Promise((resolve, reject) => {
+    request('GET', url).done((res) => {
+      if (res.statusCode == 200) {
+        resolve(JSON.parse(res.getBody('utf8')));
+      } else {
+        reject(null);
+      }
+    })
+  });
+}
 
 module.exports = router;
