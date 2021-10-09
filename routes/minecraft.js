ed78b4b4cd926b1cd0827d8706c0d57157558053
Yoichi-dev <yoichi.developer@gmail.com>
2021-10-09 20:52:09 +0900

    [Add] 利用申請通知機能追加

--- routes/minecraft.js
+++ routes/minecraft.js
@@ -5,6 +5,10 @@ let router = express.Router();
 let pinger = require('minecraft-pinger');
 let request = require('then-request');
 let common = require('../common');
+const Line = require('../notification');
+const myLine = new Line();
+require('dotenv').config();
+const ENV = process.env;
 
 router.get('/', (req, res, next) => {
   pinger.ping('minecraft.showroom-app.com', 4466, (error, result) => {
@@ -13,16 +17,18 @@ router.get('/', (req, res, next) => {
 });
 
 // ユーザ情報取得
-router.get('/search/:user_id', common.asyncWrapper(async (req, res, next) => {
+router.get('/search/:user_id/:twitter_id', common.asyncWrapper(async (req, res, next) => {
 
   try {
     let userData = await getApi(`https://api.mojang.com/users/profiles/minecraft/${req.params.user_id}`);
+    if (userData != null) {
+      myLine.setToken(ENV.LINE_API_KEY);
+      myLine.notify(`\nMinecraft Server新規利用申請\nID : ${userData.name}\nTwitter : ${req.params.twitter_id}`);
+    }
     res.json(userData);
   } catch {
-    res.json({
-      name: null,
-      id: null
-    });
+    res.status(404)
+    res.json({ error: 'UserID Not Found' })
   }
 
 }));
